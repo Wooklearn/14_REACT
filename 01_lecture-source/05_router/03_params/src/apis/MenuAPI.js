646f4616130d852c356f0a9d0d49dafbd0fd8fbe
DESKTOP-OSFKUPN\hi <rhksgns4@gmail.com>
2025-02-05 18:27:11 +0900

    [0205] router
    Signed-off-by: DESKTOP-OSFKUPN\hi <rhksgns4@gmail.com>

--- 01_lecture-source/05_router/03_params/src/apis/MenuAPI.js
+++ 01_lecture-source/05_router/03_params/src/apis/MenuAPI.js
@@ -10,8 +10,8 @@ export function getMenuList() {
 }
 
 // 전달 받은 메뉴 코드를 기반으로 메뉴 상세 조회
-export function getMenuByMenuCode({menuCode}) {
-    
+export function getMenuByMenuCode(menuCode) {
+
     //pathVariable 값은 문자열이기 때문에 정수값이 필요하면 변환 필요!
     // console.log('detail 페이지에서 넘어온 코드 : ', menuCode)
     // console.log(typeof menuCode);
